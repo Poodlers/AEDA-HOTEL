bdf0b200f6f651f304976cb5d42463e8cfd8b8b9
Joana Mesquita <joanateixeiramesquita30@gmail.com>
2020-11-05 19:47:43 +0000

    várias coisas yay

---
+++ client/client.h
@@ -0,0 +1,26 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+#include <string>
+#include <vector>
+#include "../../hotel/reservation.h"
+#include "../person.h"
+#include "../../exceptions/exceptions.h"
+
+class Client: public Person{
+public:
+    Client(const std::string &name, const unsigned int &NIF);
+
+    std::vector<Reservation*> getHistory() const;
+    void addToHistory(Reservation *reservation);
+    std::vector<Reservation*> getFutureReservations() const;
+    void addNewReservation(Reservation *reservation);
+    bool checkOut( Date *date);
+    bool checkIn( Date *date);
+    void printClient();
+
+private:
+    std::vector<Reservation*> history;
+    std::vector<Reservation*> futureReservations;
+    std::vector<Reservation*> currentReservations;
+};
+#endif
---
+++ hotel/reservation.cpp
@@ -0,0 +1,139 @@
+#include "reservation.h"
+#include <sstream>
+#include <algorithm>
+#include <iostream>
+
+int Reservation::reservationId = 0;
+
+std::ostream & Date::operator<<(std::ostream &o) {
+    o << this->day << "/" << this->month << "/" << this->year;
+    return o;
+}
+Date::Date(const int &day, const int &month, const int &year){
+    this->day = day;
+    this->month = month;
+    this->year = year;
+}
+Date::Date(const std::string& date){
+    std::stringstream ss;
+    char ignore;
+    ss<<date;
+    ss>>this->day>>ignore>>this->month>>ignore>>this->year;
+
+}
+int Date::operator - (const Date& date){
+    if (this-> year == date.getYear() && this->month == date.getMonth())
+        return this->day - date.getDay();
+    else if (this->year == date.getYear()){
+        return (this->month - date.getMonth()) * 30 - this->day + date.day ;
+    }
+    else
+        return 365 * (this->year - date.getYear()) - (this->month - date.getMonth()) * 30 - this->day + date.day;
+}
+Date::Date(){
+}
+
+int Date::getDay() const{
+    return this->day;
+}
+int Date::getMonth() const{
+    return this->month;
+}
+int Date::getYear() const{
+    return this->year;
+}
+
+bool Date::operator ==(const Date& date){
+    return ((this->day == date.getDay()) && (this->month == date.getMonth())&& (this->year == date.getYear()));
+
+}
+bool Date::operator <(const Date& date){
+    if (this->year < date.getYear()){
+        return true;
+    }
+    else if ((this->year == date.getYear()) && (this->month < date.getMonth())){
+        return true;
+    }
+    else if ((this->year == date.getYear()) && (this->month == date.getMonth()) && (this->day < date.getDay())){
+        return true;
+    }
+    return false;
+}
+bool Date::operator >(const Date& date){
+    if (this->year > date.getYear()){
+        return true;
+    }
+    else if ((this->year == date.getYear()) && (this->month > date.getMonth())){
+        return true;
+    }
+    else if ((this->year == date.getYear()) && (this->month == date.getMonth()) && (this->day > date.getDay())){
+        return true;
+    }
+    return false;
+}
+
+Reservation::Reservation(const int &reservationSize,const int& dayIn, const int&monthIn, const int &yearIn,const int& dayOut, const int&monthOut, const int &yearOut, const int & roomId){
+    Date checkIn(dayIn,monthIn,yearIn), checkOut(dayOut,monthOut,yearOut);
+    reservationId++;
+    this->reservationNumber = reservationId;
+    this->reservationSize = reservationSize;
+    this->roomId = roomId;
+    this->checkIn =checkIn;
+    this->checkOut = checkOut;
+
+}
+Reservation :: Reservation(const std::string & reservation){
+    //receives reservationSize,dayIn-monthIn-yearIn,dayOut-monthOut-yearOut,roomId
+    std::stringstream ss(reservation);
+    std::string dates;
+    int day, month, year;
+    char ignore;
+    reservationId++;
+    this->reservationNumber = reservationId;
+    ss>>this->reservationSize>> ignore >>  day >> ignore >> month   >> ignore >>  year;
+    Date dateIn(day,month,year);
+    ss>> ignore >>  day  >> ignore >>  month >> ignore >> year >>ignore >>  this->roomId;
+    Date dateOut(day,month,year);
+    this->checkIn = dateIn;
+    this->checkOut = dateOut;
+}
+
+
+Date Reservation::getCheckIn() const{
+    return this->checkIn;
+}
+
+Date Reservation::getCheckOut() const{
+    return this->checkOut;
+}
+
+int Reservation::getReservationSize() const {
+    return this->reservationSize;
+}
+
+int Reservation::getRoomId() const {
+    return this->roomId;
+}
+
+int Reservation::getReservationNumber() const {
+    return this->reservationNumber;
+}
+
+void Reservation::setReservationNumber(const int & reservationNumber){
+    this->reservationNumber =reservationNumber;
+}
+void Reservation::setReservationSize(const int & reservationSize){
+    this->reservationSize =reservationSize;
+}
+void Reservation::setCheckIn(const Date& checkIn){
+    this->checkIn =checkIn;
+}
+void Reservation::setCheckOut(const Date& checkOut){
+    this->checkOut =checkOut;
+}
+void Reservation::setRoomId(const int& roomId){
+    this->roomId =roomId;
+}
+void Reservation::print(){
+    std::cout<< "ReservationNumber: "<< this->reservationNumber << " CheckIn date: " << this->checkIn << " CheckOut date: "<< this->checkOut << " Room: " << this->roomId << " Reservation  size: " << this->reservationSize<<std::endl;
+}
